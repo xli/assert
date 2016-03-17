ad1d8d0a90747b4096c829c364196a0d10a72dd3
Xiao Li <swing1979@gmail.com>
2016-03-17 10:22:51 -0700

    rename equal to assert

--- assert_test.go
+++ assert_test.go
@@ -10,7 +10,7 @@ func ExampleTrue() {
 	True(&log, false, "msg")
 	// Output:
 	// msg
-	// equal_test.go:10:
+	// assert_test.go:10:
 	//   	var log Log
 	//   	True(&log, true, "msg")
 	// =>	True(&log, false, "msg")
@@ -24,7 +24,7 @@ func ExampleFalse() {
 	False(&log, true, "msg")
 	// Output:
 	// msg
-	// equal_test.go:24:
+	// assert_test.go:24:
 	//   	var log Log
 	//   	False(&log, false, "msg")
 	// =>	False(&log, true, "msg")
@@ -39,14 +39,14 @@ func ExampleEqual() {
 	Equal(&log, 3, "3")
 	// Output:
 	// expected 'hello', but was 'world'
-	// equal_test.go:38:
+	// assert_test.go:38:
 	//   	var log Log
 	//   	Equal(&log, 3, 3)
 	// =>	Equal(&log, "hello", "world")
 	//   	Equal(&log, 3, "3")
 	//   	// Output:
 	// expected (int), but was (string)
-	// equal_test.go:39:
+	// assert_test.go:39:
 	//   	Equal(&log, 3, 3)
 	//   	Equal(&log, "hello", "world")
 	// =>	Equal(&log, 3, "3")
@@ -62,14 +62,14 @@ func ExampleNotEqual() {
 	NotEqual(&log, 1, 1)
 	// Output:
 	// values should be different, actual: ''
-	// equal_test.go:61:
+	// assert_test.go:61:
 	//   	NotEqual(&log, "", 0)
 	//   	NotEqual(&log, "", "not")
 	// =>	NotEqual(&log, "", "")
 	//   	NotEqual(&log, 1, 1)
 	//   	// Output:
 	// values should be different, actual: 1
-	// equal_test.go:62:
+	// assert_test.go:62:
 	//   	NotEqual(&log, "", "not")
 	//   	NotEqual(&log, "", "")
 	// =>	NotEqual(&log, 1, 1)
@@ -87,7 +87,7 @@ func ExampleEqual_multilinesString() {
 	// but was : 'hello
 	// world
 	// '
-	// equal_test.go:82:
+	// assert_test.go:82:
 	//   func ExampleEqual_multilinesString() {
 	//   	var log Log
 	// =>	Equal(&log, "hello\nworld", "hello\nworld\n")
